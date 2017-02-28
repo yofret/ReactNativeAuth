e9f32350efe5e3c3539e8a4621548dc6a297a31a
Yofret Rios <yofret.rios@jobsity.com>
2017-02-28 11:09:54 -0500

    Making our input component more custom

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -9,13 +9,16 @@
 // Input Component
 //-------------------------------
 
-	const Input = ({ label, value, onChangeText }) => {
+	const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
 		const { inputStyle, labelStyle, containerStyle } = styles;
 
 		return (
 			<View style={containerStyle}>
 				<Text style={labelStyle}>{label}</Text>
 				<TextInput
+					secureTextEntry={secureTextEntry}
+					placeholder={placeholder}
+					autoCorrect={false}
 					value={value}
 					onChangeText={onChangeText}
 					style={inputStyle}
