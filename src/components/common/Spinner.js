5ac8c3bd6635c2d569ee94b0dcef30d900134665
Yofret Rios <yofret.rios@jobsity.com>
2017-02-28 11:17:36 -0500

    Adding our first spinner

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,24 @@
+//-------------------------------
+// App Dependencies 
+//-------------------------------
+
+	import React from 'react';
+	import { Text, View } from 'react-native';
+
+//-------------------------------
+// Spinner Component
+//-------------------------------
+
+	const Spinner = (props) => {
+
+		return (
+			<View>
+				<Text>Dude!!</Text>
+			</View>
+		);
+	};
+
+//-------------------------------
+// Export Component
+//-------------------------------
+	export { Spinner };
